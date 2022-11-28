9b5096a6e1235cd978bee08bdeb11efb4807114b
Lorenzo Comelli <113595270+lorenzocomelli@users.noreply.github.com>
2022-11-28 19:11:51 +0100

    Update index.php

--- FormValidation/index.php
+++ FormValidation/index.php
@@ -7,36 +7,102 @@
         <link rel="stylesheet" href="Stile.css">
     </head>
     <body>
-        <!--<p class="benvenuto"> BENVENUTO </p>-->
+        
         <?php
-         // definire le variabili e impostarle su valori vuoti
-         $nameErr = $emailErr = $genderErr = $websiteErr = "";
-         $name = $email = $gender = $comment = $website = "";
+        
+        // definire le variabili e impostarle su valori vuoti
+         $nomeErr = $emailErr = $cognErr = $pwdErr = $dataErr = $addressErr = "*";
+         $nome = $email = $cogn = $pwd = $address = "";
          
-         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (empty($_POST["name"])) {
-               $nameErr = "Campo obbligatorio";
-            }else {
-               $name = test_input($_POST["name"]);
+        function checkName(&$nome,&$nomeErr){
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (empty($_POST["nome"])) {
+                   $nomeErr = "Campo obbligatorio";
+                }else {
+                   $nome = test_input($_POST["nome"]);
+
+                   if (!preg_match("/^[a-zA-Z]{2,15}$/", $nome)) {
+                    $nomeErr = "Nome non valido.";
+                  } else {
+                    $nomeErr = "Nome valido";
+                  }
+                }
             }
-            
+        }
+
+        function checkSurname(&$cogn,&$cognErr){
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (empty($_POST["cognome"])) {
+                   $cognErr = "Campo obbligatorio";
+                }else {
+                   $cogn = test_input($_POST["cognome"]);
+
+                   if (!preg_match("/^[a-zA-Z]{2,15}$/", $cogn)) {
+                    $cognErr = "Cognome non valido.";
+                  } else {
+                    $cognErr = "Cognome valido";
+                  }
+                }
+            }
+        }
+
+        
+        function checkEmail(&$email,&$emailErr){
+        if($_SERVER["REQUEST_METHOD"] == "POST")
             if (empty($_POST["email"])) {
                $emailErr = "Campo obbligatorio";
             }else {
                $email = test_input($_POST["email"]);
                
                // controllare se l'indirizzo e-mail è corretto
-               if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                  $emailErr = "Formato mail non valido"; 
-               }
+               if (preg_match("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,6}$/", $email)) {
+                    $emailErr = "Email valida";
+                } else {
+                    $emailErr = "Email non valida";
+                }
             }
-            
-            if (empty($_POST["gender"])) {
-               $genderErr = "Campo obbligatorio";
-            }else {
-               $gender = test_input($_POST["gender"]);
+        }
+
+        function checkPassword(&$pwd,&$pwdErr){
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (empty($_POST["password"])) {
+                   $pwdErr = "Campo obbligatorio";
+                }else {
+                   $pwd = test_input($_POST["password"]);
+
+                   if (!preg_match("/^[a-zA-Z0-9._-]{8,15}$/", $pwd)) {
+                    $pwdErr = "Password non valida";
+                  } else {
+                    $pwdErr = "Password valida";
+                  }
+                }
             }
-         }
+        }
+
+        function checkData(&$dataErr){
+            if ($_SERVER["REQUEST_METHOD"] == "POST") 
+                if (empty($_POST["data"])) {
+                   $dataErr = "Campo obbligatorio";
+                }else {
+                   $dataErr = "";
+                }
+        }
+
+        function checkAddress(&$address,&$addressErr){
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                if(empty($_POST["indirizzo"])){
+                    $addressErr = "Campo Obbligatorio";
+                }else{
+                    $address = test_input($_POST["indirrizo"]);
+
+                    if (!preg_match("/^[a-zA-Z0-9' ]{2,100}+$/", $address)) {
+                        $addressErr = "Indirzzo non valido";
+                      } else {
+                        $addressErr = "Indirizzo valido";
+                      }
+                }
+            }
+        }
          
          function test_input($data) {
             $data = trim($data);
@@ -44,25 +110,38 @@
             $data = htmlspecialchars($data);
             return $data;
          }
+
+         if(isset($_POST["submit"]))
+         {
+            checkName($nome,$nomeErr,$_POST["nome"]);
+            checkSurname($cogn,$cognErr,$_POST["cognome"]);
+            checkEmail($email,$emailErr,$_POST["email"]);
+            checkSurname($pwd,$pwdErr,$_POST["password"]);
+            checkData($dataErr,$_POST["data"]);
+            checkAddress($address,$addressErr,$_POST["indirizzo"]);
+         }
         ?>
                 <h1><center><p style="color:cornflowerblue">BENVENUTO</p></center></h1>
         <div class="login-box">
             <h2>Login</h2>
             <form action=<?=$_SERVER['PHP_SELF']?> method="POST">
             <div class="user-box">
-                    <input type="text" name="name" placeholder="Name" > <span class = "error"> <?php echo $nameErr;?></span>
+                    <input type="text" name="nome" placeholder="Nome" > <span class = "error"> <?php echo $nomeErr;?></span>
                 </div><br>
                 <div class="user-box">
-                    <input type="text" name="cognome" placeholder="Cognome"> <span class = "error"> <?php echo $nameErr;?></span>
+                    <input type="text" name="cognome" placeholder="Cognome"> <span class = "error"> <?php echo $cognErr;?></span>
                 </div><br>
                 <div class="user-box">
                     <input type="text" name="email" placeholder="Email"> <span class = "error"> <?php echo $emailErr;?></span>
                 </div><br>
                 <div class="user-box">
-                    <input type="password" name="password" placeholder="Password"> <span class = "error"> <?php echo $nameErr;?></span>
+                    <input type="password" name="password" placeholder="Password"> <span class = "error"> <?php echo $pwdErr;?></span>
                 </div><br>
                 <div class="user-box">
-                    <input type="date" name="data" value="" max="2004-12-31"> <span class = "error"> <?php echo $nameErr;?></span>
+                    <input type="date" name="data" value="" max="2004-12-31"> <span class = "error"> <?php echo $dataErr;?></span>
+                </div><br>
+                <div class="user-box">
+                    <input type="text" name="indirizzo" placeholder="Indirizzo"> <span class = "error"> <?php echo $addressErr;?></span>
                 </div><br>
                     <center><input type="submit" name="submit" value="LOGIN" class="provaLogin"></center>
             </form>
